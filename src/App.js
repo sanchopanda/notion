7dffe17cd0ae1ed9546e1e09b555ff02a1d61056
sanchopanda <ivanovaa1992@gmail.com>
2020-06-16 09:36:46 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {Home} from './pages/Home'
+import {About} from './pages/About'
+import {Navbar} from './components/Navbar'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Navbar />
+    <div className="container pt-4">
+      <Switch>
+        <Route path={'/'} exact component={Home} />
+        <Route path={'/about'} component={About} />
+      </Switch>
+    </div>
+    </BrowserRouter>
+  );
+
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+export const Home = () => {
+    return (
+        <Fragment>
+            <h1>
+                Home page
+            </h1>
+        </Fragment>
+    )
+}
